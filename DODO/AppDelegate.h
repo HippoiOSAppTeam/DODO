9e6c96d15e0652d68c43a8c049e5be1ae4ceaf26
lvdong <lvdong1234@gmail.com>
2015-08-22 17:26:56 +0800

    Initial Commit

---
+++ DODO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DODO
+//
+//  Created by ld on 15/8/22.
+//  Copyright (c) 2015年 lvdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DODO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DODO
+//
+//  Created by ld on 15/8/22.
+//  Copyright (c) 2015年 lvdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
